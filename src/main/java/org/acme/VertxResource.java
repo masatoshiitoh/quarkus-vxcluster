88d8986f023a88e7e6b65d963f0be161c518e484
Masatoshi Itoh <b-itoh@devcontainer.localdomain>
2024-11-06 14:57:27 +0000

    initial commit

---
+++ src/main/java/org/acme/VertxResource.java
@@ -0,0 +1,30 @@
+package org.acme;
+
+import io.smallrye.mutiny.Uni;
+import io.vertx.mutiny.core.Vertx;
+
+import java.nio.charset.StandardCharsets;
+
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+
+@Path("/vertx")
+public class VertxResource {
+
+    private final Vertx vertx;
+
+    @Inject
+    public VertxResource(Vertx vertx) {
+        System.out.println(vertx.isClustered() ? "Clustered vertx" : "Standalone vertx");
+        
+        this.vertx = vertx;
+    }
+
+    @GET
+    @Path("/lorem")
+    public Uni<String> readShortFile() {
+        return vertx.fileSystem().readFile("lorem.txt")
+                .onItem().transform(content -> content.toString(StandardCharsets.UTF_8));
+    }
+}
