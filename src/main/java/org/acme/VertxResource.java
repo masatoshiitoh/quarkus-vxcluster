3a02f1f9089ed3c2a0af24977c2a4cb5bf10b995
Masatoshi Itoh <masatoshi9953@gmail.com>
2024-12-01 15:38:40 +0900

    Updated: cluster.xml to use tcp-ip discovery.

--- src/main/java/org/acme/VertxResource.java
+++ src/main/java/org/acme/VertxResource.java
@@ -24,6 +24,7 @@ public class VertxResource {
     @GET
     @Path("/lorem")
     public Uni<String> readShortFile() {
+        vertx.eventBus().publish("test", "Hello from Vert.x!");
         return vertx.fileSystem().readFile("lorem.txt")
                 .onItem().transform(content -> content.toString(StandardCharsets.UTF_8));
     }
